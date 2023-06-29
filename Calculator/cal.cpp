1bb7b066a2431758e114a47c8031deae15da7150
may6ix.song <smh92996275@gmail.com>
2023-06-29 21:53:26 +0900

    2번째 커밋

---
+++ Calculator/cal.cpp
@@ -0,0 +1,30 @@
+int getSum(int a, int b)
+{
+	return 0;
+}
+
+int getGop(int a, int b)
+{
+	return 0;
+}
+
+int getZegop(int a)
+{
+	return 0;
+}
+
+int getMinus(int a, int b)
+{
+	return 0;
+}
+
+int getDivide(int a, int b)
+{
+	if (b == 0) return -1;
+	else return a / b;
+}
+
+int getSumSum(int a, int b, int c)
+{
+	return 0;
+}
--- Calculator_TEST/test.cpp
+++ Calculator_TEST/test.cpp
@@ -1,6 +1,11 @@
 #include "pch.h"
+#include "../Calculator/cal.cpp"
 
 TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+	EXPECT_EQ(2, getSum(1, 1));
+	EXPECT_EQ(8, getGop(2, 4));
+	EXPECT_EQ(4, getZegop(2));
+	EXPECT_EQ(2, getMinus(3, 1));
+	EXPECT_EQ(2, getDivide(4, 2));
+	EXPECT_EQ(5, getSumSum(1, 1, 3));
 }
